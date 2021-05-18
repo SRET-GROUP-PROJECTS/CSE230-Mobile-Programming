da6e7d09a4471ba4df8585142e84eb94a5d0ada2
sivant1361 <55569255+sivant1361@users.noreply.github.com>
2021-05-18 22:36:58 +0530

    auth and home

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,29 +1,50 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+// import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
 import Login from './screens/auth/Login';
 import Signup from './screens/auth/Signup';
 import Profile from './screens/Profile';
 import Home from './screens/Home';
+import Dashboard from './screens/Dashboard';
 import Guide from './screens/guidelines';
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+// const Tabs = createBottomTabNavigator();
 const screenOptions = {
   headerShown: false,
+};
+
+const authStack = () => {
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen name="home" component={Home} />
+      <Stack.Screen name="login" component={Login} />
+      <Stack.Screen name="signup" component={Signup} />
+    </Stack.Navigator>
+  );
+};
+
+const pageStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Dashboard" component={Dashboard} />
+      <Stack.Screen name="profile" component={Profile} />
+      <Stack.Screen name="guide" component={Guide} />
+    </Stack.Navigator>
+  );
 };
 
 export const Navigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={screenOptions}>
-        <Stack.Screen name="guide" component={Guide} />
-        <Stack.Screen name="home" component={Home} />
-        <Stack.Screen name="profile" component={Profile} />
-        <Stack.Screen name="login" component={Login} />
-        <Stack.Screen name="signup" component={Signup} />
-
-      </Stack.Navigator>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Second" component={pageStack} />
+        <Drawer.Screen name="Main" component={authStack} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 };
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,43 +1,204 @@
-import React from 'react';
-import {View, Image, Text, FlatList} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Image,
+  Text,
+  ImageBackground,
+  FlatList,
+  Dimensions,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
 import CenterImg from '../images/doctornurse.png';
 import CenterImg2 from '../images/twoaunties.png';
+import BG from '../images/imagesmall.png';
 
-const Home = () => {
-  //   const images = ;
+const window_width = Dimensions.get('window').width - 40;
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 10,
+    backgroundColor: '#1133aa',
+    width: 100,
+    alignSelf: 'center',
+    marginVertical: 30,
+    borderRadius: 5,
+  },
+  center: {
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  titleContainer: {
+    width: '100%',
+    height: 100,
+    resizeMode: 'cover',
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    flexDirection: 'row',
+    alignItems: 'center',
+    elevation: 10,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 1},
+    shadowOpacity: 0.8,
+    shadowRadius: 1,
+  },
+});
+
+const Home = ({navigation}) => {
+  const [sliderState, setSliderState] = useState(true);
+
+  const buttons = [
+    <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: window_width,
+      }}>
+      <View
+        style={{
+          borderRadius: 50,
+          height: 10,
+          width: 10,
+          backgroundColor: '#8ac',
+          marginHorizontal: 5,
+          marginVertical: 5,
+        }}></View>
+      <View
+        style={{
+          borderRadius: 50,
+          height: 10,
+          width: 10,
+          backgroundColor: '#ace',
+          marginHorizontal: 5,
+          marginVertical: 5,
+        }}></View>
+    </View>,
+    <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: window_width,
+      }}>
+      <View
+        style={{
+          borderRadius: 50,
+          height: 10,
+          width: 10,
+          backgroundColor: '#ace',
+          marginHorizontal: 5,
+          marginVertical: 5,
+        }}></View>
+      <View
+        style={{
+          borderRadius: 50,
+          height: 10,
+          width: 10,
+          backgroundColor: '#8ac',
+          marginHorizontal: 5,
+          marginVertical: 5,
+        }}></View>
+    </View>,
+  ];
 
   return (
-    <View style={{padding: 20, backgroundColor: '#fff', flex: 1}}>
-      <FlatList
-        data={[
-          {
-            image: require('../images/doctornurse.png'),
-            id: 1,
-          },
-          {
-            image: CenterImg2,
-            id: 2,
-          },
-        ]}
-        style={{width: '100%'}}
-        horizontal={true}
-        pagingEnabled={true}
-        renderItem={({item, index}) => {
-          return (
-            <View style={{width: '100%'}}>
-              <Image
-                key={item.id}
-                source={item.image}
-                style={{
-                  width: '100%',
-                  height: 250,
-                }}
-              />
-              <Text>{item.id}</Text>
-            </View>
-          );
-        }}
-      />
+    <View style={{backgroundColor: '#fff', flex: 1}}>
+      <ImageBackground source={BG} style={styles.titleContainer}>
+        <Text
+          style={{
+            color: 'white',
+            fontWeight: 'bold',
+            fontFamily: 'Roboto',
+            fontSize: 30,
+            paddingHorizontal: 40,
+          }}>
+          COVAC
+        </Text>
+      </ImageBackground>
+      <View
+        style={{
+          paddingVertical: 30,
+          paddingHorizontal: 20,
+          flexDirection: 'column',
+          flex: 1,
+        }}>
+        <View style={{width: window_width, marginVertical: 10}}>
+          <FlatList
+            data={[
+              {
+                image: CenterImg,
+                id: '1',
+              },
+              {
+                image: CenterImg2,
+                id: '2',
+              },
+            ]}
+            onMomentumScrollEnd={() => {
+              setSliderState(!sliderState);
+            }}
+            keyExtractor={(item) => item.id}
+            showsHorizontalScrollIndicator={false}
+            style={{width: window_width, height: 250}}
+            horizontal={true}
+            pagingEnabled={true}
+            renderItem={({item}) => {
+              return (
+                <View style={{width: window_width}} key={item.id}>
+                  <Image
+                    source={item.image}
+                    style={{
+                      width: window_width,
+                      height: 250,
+                    }}
+                  />
+                </View>
+              );
+            }}
+          />
+          {sliderState ? buttons[0] : buttons[1]}
+        </View>
+        <View style={{marginVertical: 20}}>
+          <Text
+            style={{
+              color: '#3595e5',
+              textAlign: 'center',
+              fontSize: 23,
+              marginTop: 5,
+              paddingHorizontal: 20,
+              fontFamily: 'Roboto',
+              fontWeight: 'bold',
+              lineHeight: 30,
+            }}>
+            Better to wear a mask than a ventilator, better to stay at home than
+            in an ICU
+          </Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-around',
+            paddingHorizontal: 30,
+          }}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('login')}
+            style={styles.button}>
+            <Text style={styles.center}>Login</Text>
+          </TouchableOpacity>
+          <Text
+            style={{color: '#113344', fontWeight: 'bold', textAlign: 'center'}}>
+            OR
+          </Text>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('signup')}
+            style={styles.button}>
+            <Text style={styles.center}>Signup</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
